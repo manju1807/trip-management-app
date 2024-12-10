772685e2ce0ac2731fc2334f33e344229fe0e572
manju1807 <rmanjunath18@outlook.com>
2024-12-10 23:54:28 +0530

    fix: maps and fixed typos

--- src/hooks/useTrip.ts
+++
@@ -1,7 +0,0 @@
-import { getTripWithDetails } from '@/services';
-// hooks/useTrip.ts
-import { useMemo } from 'react';
-
-export const useTrip = (tripId: number) => {
-  return useMemo(() => getTripWithDetails(tripId), [tripId]);
-};
