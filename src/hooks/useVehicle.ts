2e3ba82ef9feb472b234e5c2c198bafd54049e11
manju1807 <rmanjunath18@outlook.com>
2024-12-10 23:53:37 +0530

    fix: maps and fixed typos

--- src/hooks/useVehicle.ts
+++
@@ -1,7 +0,0 @@
-import { getVehicleWithCurrentTrip } from '@/services';
-// hooks/useVehicle.ts
-import { useMemo } from 'react';
-
-export const useVehicle = (vehicleId: number) => {
-  return useMemo(() => getVehicleWithCurrentTrip(vehicleId), [vehicleId]);
-};
